4190d29421ab0ddebd4244ac2e5f3613a72ec234
Ubiratan Neto <118177948+UbiratanNeto@users.noreply.github.com>
2025-07-19 22:01:15 +0000

    adicionar

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,4 @@ function Home() {
   return <h1>Sexta-feira é dia de dar deploy!</h1>;
 }
 
-function teste() {
-  console.log("teste");
-}
-
-function teste2() {
-  console.log("indentação errada");
-}
 export default Home;
