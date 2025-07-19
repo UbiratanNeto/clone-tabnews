b941acbbadeaba1473a0a87acbc0e5cf62d7468e
Ubiratan Neto <118177948+UbiratanNeto@users.noreply.github.com>
2025-07-19 21:08:26 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
 function Home() {
-    return <h1>Sexta-feira é dia de dar deploy!</h1>
+  return <h1>Sexta-feira é dia de dar deploy!</h1>;
 }
 
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("indentação errada");
+}
 export default Home;
